13a5e8a8a92484663b7d5b76224656e77c8e0c7c
Huynh Phuoc Sang <sang.huynhphuoc@digitel.vn>
2021-04-02 16:15:31 +0700

    arena initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,10 +12,43 @@ var arena = (function () {
   };
   // modal
   arena.prototype.toggleModal = function () {
+    let _par = $('.modal_container');
+    var _wid = _par.width();
     $('.btn_close_modal, .js-modal').click(function() {
+      let _dataTabs = $(this).attr('data-tabs');
+      let _translateX = _wid * (parseInt(_dataTabs) - 1);
+      $('.modal_content').css('transform', 'translateX(-' + _translateX + 'px)');
       $('.js-modal_arena').toggleClass('show');
     });
+    $('.js-tabs_modals').click(function() {
+      let _dataTabs = $(this).attr('data-tabs');
+      let _translateX = _wid * (parseInt(_dataTabs) - 1);
+      $('.modal_content').css('transform', 'translateX(-' + _translateX + 'px)');
+    });
   };
+  // process bar level
+  arena.prototype.processBarLevel = function () {
+    let _process = $('.process_bar');
+    let _levelMedal = _process.attr('data-levels');
+    let _wid = _process.width() / 5;
+    let _widBack = parseFloat(_wid) * parseInt(_levelMedal);
+    _process.css({
+      'background': 'linear-gradient(90deg, #243a91 0, #6d8bff ' + _widBack + 'px, transparent ' + _widBack + 'px)'
+    });
+    let _subBar = '';
+    for (let i = 0; i < 6; i++) {
+      _subBar += '<span></span>';
+    }
+    _process.html(_subBar);
+  }
+  // process bar level
+  arena.prototype.expandGroupLevel = function () {
+    let _btn = $('.btn_expand_group');
+    _btn.click(function() {
+      $(this).toggleClass('active');
+      $(this).next().slideToggle();
+    });
+  }
   return arena;
 }());
 
